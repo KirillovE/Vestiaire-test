ec1b217221ae1f5dae540c60139f53d596645e71
Evgeniy Kirillov <eskirillov@avito.ru>
2021-08-21 14:14:16 +0300

    Initial Commit

---
+++ Vestiaire test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vestiaire test
+//
+//  Created by Евгений Кириллов on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
