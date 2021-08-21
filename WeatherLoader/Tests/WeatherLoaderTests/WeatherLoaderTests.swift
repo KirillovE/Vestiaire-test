fda097dfa4ee930abe0121fc47291d5d38364eca
Evgeniy Kirillov <eskirillov@avito.ru>
2021-08-21 17:15:09 +0300

    Response example to test data decoding

--- WeatherLoader/Tests/WeatherLoaderTests/WeatherLoaderTests.swift
+++ WeatherLoader/Tests/WeatherLoaderTests/WeatherLoaderTests.swift
@@ -1,11 +1,8 @@
-    import XCTest
-    @testable import WeatherLoader
-    
-    final class WeatherLoaderTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(WeatherLoader().text, "Hello, World!")
-        }
+import XCTest
+@testable import WeatherLoader
+
+final class EntitiesDecodingTests: XCTestCase {
+    func testExample() {
+        XCTAssertEqual(WeatherLoader().text, "Hello, World!")
     }
+}
