f924a0fadd2f66220640fc884ade521ad75387f1
Evgeniy Kirillov <eskirillov@avito.ru>
2021-08-22 15:33:05 +0300

    Specifying controller's name

--- Vestiaire test/ListViewController.swift
+++ Vestiaire test/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListViewController.swift
 //  Vestiaire test
 //
 //  Created by Евгений Кириллов on 21.08.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
