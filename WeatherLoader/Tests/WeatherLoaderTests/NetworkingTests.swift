a1e02dfda66e3988aac1446a65d14a57aad30760
Evgeniy Kirillov <eskirillov@avito.ru>
2021-09-03 12:22:41 +0300

    Reducing code duplication

--- WeatherLoader/Tests/WeatherLoaderTests/NetworkingTests.swift
+++ WeatherLoader/Tests/WeatherLoaderTests/NetworkingTests.swift
@@ -3,13 +3,18 @@ import XCTest
 
 final class NetworkingTests: XCTestCase {
     
-    func testFormURL() {
-        let loader = Loader(
+    private var loader: Loader?
+    
+    override func setUp() {
+        loader = Loader(
             appID: "vestiaire",
             mode: .json,
             units: .metric
         )
-        let url = loader.formURL(1, "Paris")
+    }
+    
+    func testFormURL() {
+        let url = loader?.formURL(1, "Paris")
         XCTAssertNotNil(url)
         XCTAssertEqual(url!.scheme, "https")
         XCTAssertEqual(url!.host, "api.openweathermap.org")
@@ -24,28 +29,19 @@ final class NetworkingTests: XCTestCase {
     }
     
     func testCountNormalizationGreater() {
-        let loader = Loader(
-            appID: "vestiaire",
-            mode: .json,
-            units: .metric
-        )
-        let url = loader.formURL(100, "Paris")
+        let url = loader?.formURL(100, "Paris")
         let queryParameters = getQueryParameters(of: url!)
         
         XCTAssertEqual(queryParameters["cnt"], "16")
     }
     
     func testCountNormalizationLess() {
-        let loader = Loader(
-            appID: "vestiaire",
-            mode: .json,
-            units: .metric
-        )
-        let url = loader.formURL(-7, "Paris")
+        let url = loader?.formURL(-7, "Paris")
         let queryParameters = getQueryParameters(of: url!)
         
         XCTAssertEqual(queryParameters["cnt"], "1")
     }
+    
     
 }
 
